94d4a36cb7c0d1090eb3b7144c78be311211d7cc
Numan Laanait <laanaitn@ornl.gov>
2019-04-29 10:17:45 -0400

    adding scripts to generate spacegroup classification for SMC19

---
+++ scripts/summit_scripts/smc_19_sim.py
@@ -0,0 +1,182 @@
+from namsa import SupercellBuilder, MSAGPU
+from utils import *
+import numpy as np
+from time import time
+import sys, os, re
+import h5py
+from mpi4py import MPI
+from itertools import chain, product
+import tensorflow as tf
+import lmdb
+
+comm = MPI.COMM_WORLD
+comm_size = comm.Get_size()
+comm_rank = comm.Get_rank()
+
+
+
+def simulate(filehandle, cif_path, idx= None, gpu_id=0, clean_up=False):
+    # load cif and get sim params
+    spgroup_num, matname = parse_cif_path(cif_path)
+    sp = SupercellBuilder(cif_path, verbose=False, debug=False)
+    sim_params = get_sim_params(sp, grid_steps=np.array([8,8]), orientation_num=3)
+    z_dirs = sim_params['z_dirs']
+    y_dirs = sim_params['y_dirs']
+    cell_dim = sim_params['cell_dim']
+    slab_t = sim_params['slab_t']
+    sim_params['space_group']= spgroup_num
+    sim_params['material'] = matname
+    energies = [100, 125, 150, 175, 200]
+    for (sample_idx, energy) in enumerate(energies):
+        try:
+            cbed_stack = []
+            for y_dir, (z_idx, z_dir) in zip(y_dirs, enumerate(z_dirs)):
+                t = time()
+                # build supercell
+                sp.build_unit_cell()
+                sp.make_orthogonal_supercell(supercell_size=np.array([cell_dim,cell_dim,slab_t]),
+                                 projec_1=y_dir, projec_2=z_dir)
+
+                # set simulation params
+                slice_thickness = sim_params['d_hkl'][z_idx]
+                semi_angle= sim_params['semi_angles'][z_idx]
+                probe_params = sim_params['probe_params']
+                sampling = sim_params['sampling']
+                grid_steps = sim_params['grid_steps']
+
+                # simulate
+                msa = MSAGPU(energy, semi_angle, sp.supercell_sites, sampling=sampling,
+                     verbose=False, debug=False)
+                ctx = msa.setup_device(gpu_rank=gpu_id)
+                msa.calc_atomic_potentials()
+                msa.build_potential_slices(slice_thickness)
+                msa.build_probe(probe_dict=probe_params)
+                msa.generate_probe_positions(grid_steps=grid_steps) 
+                msa.plan_simulation()
+                msa.multislice(bandwidth=1./2)
+
+                # process cbed 
+                cbed = msa.probes.mean(0) 
+
+                # update sim_params dict
+                sim_params = update_sim_params(sim_params, msa_cls=msa, sp_cls=sp)
+
+                # 
+                # clean-up context and/or allocated memory
+                if clean_up and ctx is not None:
+                    msa.clean_up(ctx=ctx, vars=msa.vars)
+                    del msa
+                else:
+                    msa.clean_up(ctx=None, vars=msa.vars)
+                    del msa
+
+                # check data integrity
+                has_nan = np.all(np.isnan(cbed)) 
+                wrong_shape = cbed.shape != (512, 512) 
+                if has_nan: 
+                    print('rank=%d, skipped simulation=%s, index=%d, error=NaN' % (comm_rank, cif_path, sample_idx))
+                    break
+                elif wrong_shape:
+                    print('rank=%d, skipped simulation=%s, index=%d, error=wrong cbed shape' % (comm_rank, cif_path, sample_idx))
+                    break
+                else:
+                    cbed_stack.append(cbed)
+
+
+            # write to h5 / tfrecords / lmdb
+            if len(cbed_stack) != 3:
+                break
+            else:
+                cbed_stack = np.stack(cbed_stack)
+            if cbed_stack.shape != (3, 512, 512):
+                print('rank=%d, skipped simulation=%s, index=%d, error=Wrong cbed_stack Shape' % (comm_rank, cif_path, sample_idx))
+                break
+            else:
+                g = filehandle.create_group('sample_%d_%d' % (idx, sample_idx))
+                g.attrs['space_group'] = np.string_(sim_params['space_group'])
+                g.attrs['material'] = np.string_(sim_params['material'])
+                g.attrs['energy_keV'] = energy
+                dset = g.create_dataset('cbed_stack', data=cbed_stack)
+                g.attrs['z_dirs'] = np.dstack(z_dirs)
+                g.attrs['y_dirs'] = np.dstack(y_dirs)
+                g.attrs['semi_angles_rad'] = sim_params['semi_angles']
+                g.attrs['d_hkls_angstrom'] = sim_params['d_hkl']
+                g.attrs['abc_angstrom'] = sim_params['abc']
+                g.attrs['angles_degree'] = sim_params['angles']
+                g.attrs['formula'] = sim_params['formula'] 
+                filehandle.flush() 
+                print('rank=%d, finished simulation=%s, index=%d' % (comm_rank, cif_path, sample_idx))
+        except Exception as e:
+            print("rank=%d, skipped simulation=%s, error=%s" % (comm_rank, cif_path, format(e)))
+        finally:
+            try:
+                if clean_up and ctx is not None:
+                    msa.clean_up(ctx=ctx, vars=msa.vars)
+                    del msa
+                else:
+                    msa.clean_up(ctx=None, vars=msa.vars)
+                    del msa
+            except:
+                pass
+        
+
+def generate_data(samples, outdir_path, mode='train', runtime=2000):
+    t = time()
+    num_sims = samples.size
+    h5path = os.path.join(outdir_path, 'batch_%s_%d.h5'% (mode, comm_rank))
+    mode = 'w'
+    with h5py.File(h5path, mode=mode) as f:
+        for (idx, cif_path) in enumerate(samples[comm_rank:num_sims:comm_size]):
+            manual = idx < ( num_sims - comm_size) 
+            spgroup_num, matname = parse_cif_path(cif_path)
+            if comm_rank == 0 and bool(idx % 100):
+                print('time=%3.2f, num_sims= %d' %(time() - t, idx * comm_size))
+            if (time() - t_elaps) < runtime:
+                simulate(f, cif_path, idx=idx, gpu_id=int(np.mod(comm_rank, 6)), clean_up=manual)
+            else:
+#                 f.flush()
+                break
+            
+def get_samples(cif_paths, ratio=0.9):
+    samples = cif_paths
+    if comm_rank == 0:
+        indices = np.arange(0,samples.size, dtype=np.int64) 
+        np.random.shuffle(indices)
+    else:
+        indices = np.empty(samples.size, dtype=np.int64)
+    comm.Bcast(indices, root=0)
+    samples = samples[indices]
+    if ratio is not None:
+        train_size = int(samples.size * ratio)
+        remain = (1 - ratio)/2
+        samples_train = samples[:train_size]
+        samples_dev = samples[train_size:train_size + int(remain * train_size)]
+        samples_test = samples[train_size + int(remain * train_size):]
+        print('samples sizes (train, dev, test): %d, %d, %d' %(samples_train.size, samples_dev.size, samples_test.size))
+        return samples_train, samples_dev, samples_test
+    return samples
+         
+def main(cifdir_path, outdir_path, runtime=1800):
+    global t_elaps
+    t_elaps = time()
+    cif_paths = get_cif_paths(cifdir_path)
+    samples_train, samples_dev, samples_test = get_samples(cif_paths, ratio=0.9)
+    generate_data(samples_train, outdir_path, mode='train', runtime=runtime*0.9)
+    print('rank=%d, finished simulating training data' % comm_rank)
+    generate_data(samples_test, outdir_path, mode='dev', runtime=runtime*0.95)
+    print('rank=%d, finished simulating dev data' % comm_rank)
+    generate_data(samples_dev, outdir_path, mode='test', runtime=runtime)
+    print('rank=%d, finished simulating test data' % comm_rank)
+    return
+            
+def main_test(cifdir_path):
+    cifpaths_train, cifpaths_test= get_cif_paths(cifdir_path, ratio=0.2)
+    print("train", cifpaths_train[:10])
+    print("test", cifpaths_test[:10])
+                
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        cifdir_path, outdir_path, runtime = sys.argv[-3:]
+        main(cifdir_path, outdir_path, runtime=int(runtime))
+    else:
+        print("Pass directory paths for sim input files and h5 output files")
---
+++ scripts/summit_scripts/smc_19_utils.py
@@ -0,0 +1,209 @@
+import numpy as np
+import sys, os, re
+from itertools import chain
+from namsa.scattering import get_kinematic_reflection, overlap_params 
+from namsa.optics import voltage2Lambda
+import tensorflow as tf
+import h5py
+from scipy.ndimage import gaussian_filter
+from skimage.transform import resize
+from scipy.optimize import minimize
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _feature_fetch(cbed, potential, params):
+    '''
+    Returns a tf.train.Example that is written by TFRecordwriter
+    '''
+    example = tf.train.Example(features=tf.train.Features(feature={
+
+                        #labels
+                        'material':_bytes_feature(np.array(params['material']).tostring()),
+                        'space_group': _bytes_feature(np.array(params['space_group']).tostring()),
+                        'abc': _bytes_feature(np.array(params['abc']).tostring()),
+                        'angles': _bytes_feature(np.array(params['angles']).tostring()),
+                        'formula': _bytes_feature(np.array(params['formula']).tostring()),
+
+                        #potential
+                        '2d_potential': _bytes_feature(potential),
+          
+                        #image
+                        'cbed': _bytes_feature(cbed)}))
+    return example 
+
+def pop_DS(lst):
+    for (i,itm) in enumerate(lst):
+        if '.DS_Store' in itm:
+            lst.pop(i)
+
+def get_cif_paths(root_path, ratio=None):
+    space_group_dirs = os.listdir(root_path)
+    pop_DS(space_group_dirs)
+    cifpath_list = []
+    for spg_dir in space_group_dirs:
+        cif_list = os.listdir(os.path.join(root_path,spg_dir))
+        pop_DS(cif_list)
+        cif_paths = [os.path.join(os.path.join(root_path,spg_dir),cif_name) for cif_name in cif_list]
+        cifpath_list.append(cif_paths)
+    cifpath_list = list(chain.from_iterable(cifpath_list))
+    cifpath_list = np.array(cifpath_list)
+    return cifpath_list 
+
+def get_cell_orientation(vec):
+    def func(x):
+        return np.abs(np.dot(vec,x))
+    res = minimize(func, np.array([-1,1,-1]), method='CG')
+    if np.abs(res.x.sum()) < 1e-4:
+        return np.array([0,0,1])
+    return res.x
+    
+
+def parse_cif_path(cif_path):
+    spgroup, matname = cif_path.split(os.sep)[-2:]
+    matname = matname.split('.')[0]
+    spgroup_num = re.findall('\d+',spgroup)[0]
+    return spgroup_num, matname
+
+def write_h5(h5group, cbed, potential, params):
+    # try:
+    num_itms = len(h5group.items())
+    g = h5group.create_group('sample_%d' % num_itms)
+    dset_cbed = g.create_dataset('CBED',  data=cbed)
+    dset_pot = g.create_dataset('potential', data=potential)
+    # need to figure out how to assign attributes to each dset and the parent group.
+    # now dumping everything into group attribute
+    for key in params.keys():
+        if key == 'probe_params':
+            for probe_key in params['probe_params']:
+                if probe_key is 'aberration_dict':
+                    aberr = params['probe_params']['aberration_dict']
+                    g.attrs['aberrations']= np.array([aberr['C1'], aberr['C3'], aberr['C5']])
+                else:
+                    g.attrs[probe_key] = params['probe_params'][probe_key]
+        else:
+            g.attrs[key] = params[key]
+    return
+
+def write_tfrecord(tfrecord_writer, cbed, potential, params):
+    #serialize image
+    cbed = cbed.tostring()
+    potential = potential.tostring()
+    example = _feature_fetch(cbed, potential, params)
+    tfrecord_writer.write(example.SerializeToString()) 
+    return
+
+def write_lmdb(txn, idx, cbed, potential, record_names=["2d_potential_", "cbed_"], params=None):
+    # barebone writing to file
+    key = bytes('%s%s' %(record_names[0],format(idx)), "ascii")
+    sample = potential.flatten()
+    sample = sample.tostring()
+    txn.put(key, sample)
+    key = bytes('%s%s' %(record_names[1],format(idx)), "ascii")
+    sample = cbed.flatten()
+    sample = cbed.tostring()
+    txn.put(key, sample)
+    
+    # need to figure out how to write params for each sample
+    return
+
+def process_potential(pot_slices, normalize=True, mask=None, sampling=None, scale=[-1,1], expand_dim=True, fp16=False):
+    proj_potential = np.imag(pot_slices).mean(0)
+    proj_potential = gaussian_filter(proj_potential,1.2)
+    snapshot = slice(int(proj_potential.shape[0]// 4), int(3 * proj_potential.shape[1]//4))
+    proj_potential = proj_potential[snapshot, snapshot]
+    proj_potential = resize(proj_potential,sampling, preserve_range=True, mode='constant', order=4)
+    #if mask is None:
+    #    pass
+        #mask = np.ones((sampling, sampling), dtype=np.bool)
+        #snapshot = slice(int(proj_potential.shape[0]// 4), int(3 * proj_potential.shape[1]//4))
+        #mask[snapshot, snapshot] = False
+    #else:
+    #    mask = np.logical_not(mask)
+    #    proj_potential[mask] = 0
+    proj_potential = (proj_potential - proj_potential.mean())/max(proj_potential.std(), 1./np.sqrt(proj_potential.size)) 
+    #proj_potential = proj_potential - proj_potential.min()
+    #proj_potential = (proj_potential - proj_potential.min())/(proj_potential.max() - proj_potential.min())
+    #proj_potential = proj_potential * (scale[-1] - scale[0]) + scale[0]
+    proj_potential = np.expand_dims(proj_potential, axis=0)
+    proj_potential = proj_potential.astype(np.float16)
+    #if normalize:
+    #    proj_potential = (proj_potential - proj_potential.mean())/max(proj_potential.std(), 1./np.sqrt(proj_potential.size)) 
+    #if expand_dim:
+    #    proj_potential = np.expand_dims(proj_potential, axis=0)
+    #if fp16:
+    #    return proj_potential.astype(np.float16)
+    return proj_potential
+
+def process_cbed(cbed, normalize=True, scale=[-1, 1], fp16=False):
+    #cbed = np.sqrt(cbed)
+    #cbed = cbed ** (1./3)
+    cbed = (cbed - np.mean(cbed, axis=(1,-1), keepdims=True))/np.std(cbed, axis=(1,-1), keepdims=True)
+    #cbed = (cbed - np.min(cbed, axis=(1,-1), keepdims=True))/(np.max(cbed, axis=(1,-1), keepdims=True) - np.min(cbed, axis=(1,-1), keepdims=True))
+    #cbed = cbed * (scale[-1] - scale[0]) + scale[0]
+    cbed = cbed.astype(np.float16)
+    #if normalize:
+    #    cbed = cbed ** (1/3)
+    #    cbed = (cbed - cbed.mean())/max(cbed.std(), 1./np.sqrt(cbed[0].size)) 
+    #if fp16:
+    #    return cbed.astype(np.float16)
+    return cbed
+
+def update_sim_params(sim_params, msa_cls=None, sp_cls=None):
+    # msa params
+    if msa_cls is not None:
+        msa_params = ['max_ang', 'kmax', 'debye_waller', 'dims', 'kpix_size', 'pix_size', 'sigma']
+        for key in msa_params:
+            try:
+                val = msa_cls.__getattribute__(key)
+                sim_params[key] = val
+            except Exception as e:
+                sim_params[key] = str(e)
+    if sp_cls is not None:
+        try:
+            sim_params['formula'] = sp_cls.structure.formula
+            sim_params['abc'] = sp_cls.structure.lattice.abc
+            sim_params['angles'] = sp_cls.structure.lattice.angles
+        except Exception as e:
+            sim_params['formula'] = str(e)
+            sim_params['abc'] = str(e)
+            sim_params['angles'] = str(e)
+    return sim_params
+
+def get_sim_params(sp_cell, slab_t= 200, sampling=np.array([512,512]), d_cutoff=4, grid_steps=np.array([32, 32]),
+                   cell_dim = 100, energy=100e3, orientation_num=3, beam_overlap=1):
+    """
+    return a dict object to set params of simulation and write to h5.
+    """
+    sim_params= dict()
+    
+    # scattering params
+    hkls, dhkls = get_kinematic_reflection(sp_cell.structure, top=orientation_num)
+    if hkls[0].size > 3: # hexagonal systems    
+        hkls = np.array([[itm[0], itm[1], itm[-1]] for itm in hkls])
+    cutoff = np.logical_and(dhkls < 5., dhkls > 1.) # not considering less than 5 ang. d-spacing
+#     if dhkls[cutoff].size > 1:
+    hkls, dhkls = hkls[cutoff], dhkls[cutoff]
+    y_dirs = np.array([get_cell_orientation(z_dir) for z_dir in hkls])
+    semi_angles, _, _ = overlap_params(0.5, dhkls, voltage2Lambda(energy))
+    sim_params['y_dirs'] = y_dirs
+    sim_params['z_dirs'] = hkls
+    sim_params['semi_angles'] = semi_angles * 1e-3 # mrad
+    sim_params['d_hkl'] = dhkls
+    sim_params['cell_dim'] = cell_dim # ang.
+    sim_params['energy'] = energy * 1e-3 # keV
+    sim_params['sampling'] = sampling
+    sim_params['slab_t'] = slab_t # ang.
+    sim_params['grid_steps'] = grid_steps
+    
+    # optics params
+    sim_params['probe_params'] = {'smooth_apert': True, 'scherzer': False, 'apert_smooth': 30, 
+                'aberration_dict':{'C1':0., 'C3':0 , 'C5':0.}, 'spherical_phase': True}
+    return sim_params
+
+def get_slice_thickness(sp_cell, direc=np.array([0,0,1])):
+    hkls, dhkls = get_kinematic_reflection(sp_cell.structure,top=10)
+    if hkls[0].size > 3: # hexagonal systems    
+        hkls = np.array([[itm[0], itm[1], itm[-1]] for itm in hkls])
+    idx = np.argmin(np.abs(np.cross(hkls,direc).sum(1)))
+    return dhkls[idx]
